22ec16a4e8063b968d8db97ce049d2e17a3aa088
Keith McDuffee <kmcduffee@stackref.com>
2024-07-18 09:30:28 -0400

    feat(ollama-entrypoint.sh): add startup message and pull nomic-embed-text model
    - Add a startup message to indicate the entrypoint script is running.
    - Implement a new function to pull the nomic-embed-text model after the mistral model has been pulled, to ensure that both models are available for use.

--- ollama-entrypoint.sh
+++ ollama-entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+echo ":: Starting custom Ollama entrypoint"
+
+echo ":: Pulling mistral model"
 /bin/ollama pull mistral &
 
 pull1_pid=$!
 
 wait_and_pull() {
     wait $pull1_pid
+    echo ":: Pulling nomic-embed-text model"
     /bin/ollama pull nomic-embed-text
 }
 
